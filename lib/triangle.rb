7926155182d409ee06698019beb910173baccf78
Elle Faraday <faraday.elle@gmail.com>
2017-11-18 19:28:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,22 @@ class Triangle
   end
 
   def kind
-    if @side_a == @side_b && @side_b == @side_c
+    if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 || (@side_a + @side_b) <= @side_c || (@side_c + @side_b) <= @side_a || (@side_a + @side_c) <= @side_b
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a == @side_b || @side_b == @side_c || @side_c == @side_a
       :isosceles
     else
       :scalene
     end
+  end
+
+  class TriangleError < StandardError
+    "Triangles must have 3 non-negative sides. The sum of any two sides must be greater than the 3rd side"
   end
 end
